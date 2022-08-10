5e893d5d7e75014f9cd575e00d18f2f690cf75fc
str_hc <fbpetrella@gmail.com>
2022-08-10 16:50:35 +0200

    responsive

--- src/StyledComponents/navLinks.styled.js
+++ src/StyledComponents/navLinks.styled.js
@@ -11,22 +11,28 @@ export const NavLinks = styled(Link)`
   position: relative;
 
   &::after {
-      background: none repeat scroll 0 0 transparent;
-      content: "";
-      display: block;
-      height: 2px;
-      bottom: -2px;
-      left: 50%;
-      position: absolute;
-      background: rgba(255, 111, 0, 1);
-      transition: width 0.3s ease 0s, left 0.3s ease 0s;
-      width: 0;
-    }
-    &:hover {
-      color: rgba(255, 111, 0, 1);
-    }
-    &:hover::after {
-      width: 100%;
-      left: 0;
-    }
+    background: none repeat scroll 0 0 transparent;
+    content: "";
+    display: block;
+    height: 2px;
+    bottom: -2px;
+    left: 50%;
+    position: absolute;
+    background: rgba(255, 111, 0, 1);
+    transition: width 0.3s ease 0s, left 0.3s ease 0s;
+    width: 0;
+  }
+  &:hover {
+    color: rgba(255, 111, 0, 1);
+  }
+  &:hover::after {
+    width: 100%;
+    left: 0;
+  }
+  @media (max-width: 768px) {
+    color: #ff6f00da;
+    font-weight: bold;
+    background-color: rgba(0, 0, 0, 0.3);
+    padding: 1rem 1rem;
+  }
 `;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLinks } from "../StyledComponents/navLinks.styled.js";
 import logo from "../img/logo_small_orange.png";
-
+import { FaBars } from "react-icons/fa";
 
 function Navbar() {
+  const [mobileMenu, setMobileMenu] = useState("hidden mobile-links");
   return (
-    <nav className="navbar">
-      <div className="logo">
-        
+    <>
+      <nav className="navbar">
+        <div className="logo">
           <img src={logo} alt="logo" />
-        
-      </div>
-      <div>
-        <NavLinks to="/">About Me</NavLinks>
-        <NavLinks to="/projects">Projects</NavLinks>
-        <NavLinks to="/contact">Contact</NavLinks>
-      </div>
-    </nav>
+        </div>
+        <div className={`mobile-links ${mobileMenu}`}>
+          <div
+            className="nav-links"
+            onClick={
+              mobileMenu === "visible"
+                ? () => setMobileMenu("hidden")
+                : () => setMobileMenu("visible")
+            }
+          >
+            <NavLinks
+              to="/"
+              onClick={
+                mobileMenu === "visible"
+                  ? () => setMobileMenu("hidden")
+                  : () => setMobileMenu("visible")
+              }
+            >
+              About Me
+            </NavLinks>
+            <NavLinks
+              to="/projects"
+              onClick={
+                mobileMenu === "visible"
+                  ? () => setMobileMenu("hidden")
+                  : () => setMobileMenu("visible")
+              }
+            >
+              Projects
+            </NavLinks>
+            <NavLinks
+              to="/contact"
+              onClick={
+                mobileMenu === "visible"
+                  ? () => setMobileMenu("hidden")
+                  : () => setMobileMenu("visible")
+              }
+            >
+              Contact
+            </NavLinks>
+          </div>
+        </div>
+        <FaBars
+          className="fabars"
+          onClick={
+            mobileMenu === "visible"
+              ? () => setMobileMenu("hidden")
+              : () => setMobileMenu("visible")
+          }
+        />
+      </nav>
+    </>
   );
 }
 
