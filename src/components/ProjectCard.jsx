301bddac95525ff22e40ab81be7bcc8c01009a2f
str_hc <fbpetrella@gmail.com>
2022-11-13 20:46:22 +0100

    fixed layout

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,17 +1,15 @@
-import React from "react";
-import "animate.css";
+import React from 'react';
+import 'animate.css';
 
-import { GoCode } from "react-icons/go";
-import { IoMdOpen } from "react-icons/io";
+import { GoCode } from 'react-icons/go';
+import { IoMdOpen } from 'react-icons/io';
 
-
-import a01 from "../img/01mini.gif";
-import a02 from "../img/02sight.gif";
-import a03 from "../img/03bulls.gif";
-import a04 from "../img/04bur.gif";
-import a06 from "../img/06evcon.gif";
-import a07 from "../img/07exgen.gif";
-
+import a01 from '../img/01mini.gif';
+import a02 from '../img/02sight.gif';
+import a03 from '../img/03bulls.gif';
+import a04 from '../img/04bur.gif';
+import a06 from '../img/06evcon.gif';
+import a07 from '../img/07exgen.gif';
 
 function ProjectCard({ props }) {
   const imgArray = [a02, a06, a03, a01, a04, a07];
@@ -32,17 +30,34 @@ function ProjectCard({ props }) {
         <div className="toolsCont">
           {props.info !== undefined &&
             props.info.map((item, idx) => (
-              <div className="tools" key={idx + ""}>
+              <div className="tools" key={idx + ''}>
                 {item}
               </div>
             ))}
         </div>
 
         <div className="ext-linkContainer">
-        {props.github &&  <a href={props.github} target="_blank" rel="noreferrer" className="ext-link"><GoCode/></a>}
-       
-          {props.extLink && <a href={props.extLink} target="_blank" rel="noreferrer" className="ext-link"><IoMdOpen /></a>}
-          
+          {props.github && (
+            <a
+              href={props.github}
+              target="_blank"
+              rel="noreferrer"
+              className="ext-link"
+            >
+              <GoCode />
+            </a>
+          )}
+
+          {props.extLink && (
+            <a
+              href={props.extLink}
+              target="_blank"
+              rel="noreferrer"
+              className="ext-link"
+            >
+              <IoMdOpen />
+            </a>
+          )}
         </div>
       </div>
     </>
